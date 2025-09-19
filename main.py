68218768877d7d6685bd27047fbb4e12f456b633
Arikapa <stevenaricapa@outlook.es>
2025-09-19 08:10:37 -0500

    Colocando la suma

--- main.py
+++ main.py
@@ -3,5 +3,9 @@ print("Hola, " + nombre)
 print(f"Hola, {nombre}")
 nombre = "Mundo"
 print("Hola, " + nombre)
-
 print("Soy Arikapa")
+print("---------------------------------")
+print("SUMA")
+num1 = 5
+num2 = 3    
+print(num1 + num2)
